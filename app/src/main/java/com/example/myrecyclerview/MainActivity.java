1f48d6425c1f50983b27c7f0d9d3a0a8ecef585f
aulialigar <aulialsh69@gmail.com>
2019-09-09 15:16:48 +0700

    list dan grid

--- app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -47,11 +48,22 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                showRecyclerList();
                 break;
+
             case R.id.action_grid:
+                showRecyclerGrid();
                 break;
+
             case R.id.action_cardview:
+
                 break;
         }
+    }
+
+    private void showRecyclerGrid() {
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
     }
 }
